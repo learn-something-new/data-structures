dd52aac5d2e707722051f8a047387490675d8224
Jeff Hann <jeffhann@gmail.com>
2014-11-25 17:38:01 -0400

    stacks!

--- stack.cpp
+++ stack.cpp
@@ -18,10 +18,61 @@ class stack
             // set top location to -1 indicating a empty stack
             top = -1;
         }
+
+        // add to the stack
+        void push(int a)
+        {
+            // increment top
+            top++;
+
+            // if top is below limit go ahead and add
+            // else cout an error and de-increment top
+            if(top<MAX)
+            {
+                arr[top] = a;
+            }
+            else
+            {
+                cout << "STACK FULL!" << endl;
+                top--;
+            }
+        }
+
+        // delete from top of stack and return item
+        int pop()
+        {
+            // if top is -1 the stack is empty so cout an error
+            // else remove the top item from the stack
+            if(top == -1)
+            {
+                cout << "STACK IS EMPTY!" << endl;
+
+                return 0;
+            }
+            else
+            {
+                int data = arr[top];
+                arr[top] = 0;
+                top--;
+
+                return data;
+            }
+        }
 };
 
 int main()
 {
     stack a;
+
+    // push the numbers 3, 5, and 2
+    a.push(3);
+    a.push(5);
+    a.push(2);
+
+    // pop all 3 values and cout to the user
+    cout << a.pop() << " was popped" << endl;
+    cout << a.pop() << " was popped" << endl;
+    cout << a.pop() << " was popped" << endl;
+
     return 0;
 };
