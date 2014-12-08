6a2cca25962574ef7dcfbc19980ec2a512c8fd22
Jeff Hann <jeffhann@gmail.com>
2014-12-08 14:59:29 -0400

    updating make so we dont include debug flags when making the entire directory increasing count of numbers in tree to 100

--- avl/main.cpp
+++ avl/main.cpp
@@ -10,13 +10,13 @@ int main()
 
     srand(time(NULL));
 
-    for(int x = 0; x <= 10; x++)
+    for(int x = 0; x <= 100; x++)
     {
         int num = (rand() % 100) + 1;
         std::cout << num << " ";
         avl->add(num);
     }
-    std::cout << std::endl;
+    std::cout << std::endl << std::endl;
 
     avl->display();
 
